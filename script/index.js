b58dfab19c5d8ef4f0bad4e611913a1cf533747a
lorenzobose <lorybose06@gmail.com>
2024-04-08 22:28:12 +0200

    Aggiunta File V.1

---
+++ script/index.js
@@ -0,0 +1,57 @@
+//DICHIARAZIONE DEI TAG
+const field = document.createElement("div");
+const body = document.querySelector("body");
+const title = document.createElement("h1");
+const scelta = document.createElement("p");
+const input = document.createElement("input");
+const Invio  = document.createElement("a");
+
+function setup(){
+
+}
+
+
+function check(){
+  console.log(input.value)
+}
+
+
+//IMPLEMENTAZIONE DEL FIELD
+field.id = "field";
+body.appendChild(field);
+
+//HEADER DELL' APP
+title.innerText = "Scrivi la traduzione......"
+field.appendChild(title);
+
+//Blocco DEL PRIMO TESTO
+const div1 = document.createElement("div");
+div1.appendChild(scelta);
+field.appendChild(div1);
+
+//BLOCCO DELLA TRADUZIONE
+const div2 = document.createElement("div");
+div2.appendChild(input);
+
+input.addEventListener("keypress", function(event){if (event.key == "Enter"){check;}});
+
+Invio.textContent = "Invio";
+Invio.onclick = check;
+div2.appendChild(Invio); 
+field.appendChild(div2);
+
+const fit = () => {
+    if(innerWidth > 1000){
+      field.classList = [];
+      field.classList.add("PCField");
+    }
+    else{
+      field.classList = [];
+      field.classList.add("MPField");
+    }
+  };
+  
+document.addEventListener("DOMContentLoaded", fit);
+window.addEventListener("resize", fit);
+document.addEventListener("fullscreenchange", fit);
+  
